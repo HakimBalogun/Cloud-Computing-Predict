85279e7289211bcef1dbfed1086bd88b4e5ea0ae
Hakim Balogun <99412936+HakimBalogun@users.noreply.github.com>
2022-05-06 03:07:13 +0100

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "arn:aws:execute-api:eu-west-1:861067433899:44erkjkh38/*/POST/apiresource", 
+                url: "https://k03etrm5r1.execute-api.eu-west-1.amazonaws.com/default/Portfolio_Model_Solution", 
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
